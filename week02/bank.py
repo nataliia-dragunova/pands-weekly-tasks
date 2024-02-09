344a43fa0d4be6558f2ba25c0199fc0f1cc1519c
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-09 23:31:40 +0000

    week 02

---
+++ week02/bank.py
@@ -0,0 +1,15 @@
+# bank.py
+# Prompt the user and read in two money amounts (in cent)
+# Add the two amounts
+# out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
+# author : Nataliia Dragunova
+
+amount1 = 65
+amount2 = 180
+print ("The sum of these is €" + str(amount1 + amount2)/100);
+
+'''
+amount1 = int(input("please enter first amount in cents:"))
+amount2 = int(input("please enter second amount in cents:"))
+print ("The sum of these is €" + str(amount1 + amount2)/100);
+'''

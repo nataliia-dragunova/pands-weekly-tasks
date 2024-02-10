3f04f4ba3f9435622a35e0ff78cde04ee297378f
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-10 11:14:48 +0000

    bank

--- week02/bank.py
+++ week02/bank.py
@@ -4,12 +4,17 @@
 # out the answer in a human readable format with a euro sign and decimal point between the euro and cent of the amount 
 # author : Nataliia Dragunova
 
+'''
+# first attempt with exact amounts after reading "Python String Formatting (w3schools.com)"
 amount1 = 65
 amount2 = 180
-print ("The sum of these is €" + str(amount1 + amount2)/100);
+sum = (amount1 + amount2)/100
+txt = "The sum of these is €{}"
+print (txt.format(sum));
 
 '''
 amount1 = int(input("please enter first amount in cents:"))
 amount2 = int(input("please enter second amount in cents:"))
-print ("The sum of these is €" + str(amount1 + amount2)/100);
-'''
+sum = (amount1 + amount2)/100
+txt = "The sum of these is € {}"
+print (txt.format(sum));

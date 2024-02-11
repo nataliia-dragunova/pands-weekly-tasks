b4e6cde1603eb214461785775149ccf372d63923
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-11 16:16:35 +0000

    task03

--- task03/accounts.py
+++ task03/accounts.py
@@ -3,3 +3,9 @@
 # outputs the account number with only the last 4 digits showing
 # the first 6 digits replaced with Xs
 # author : Nataliia Dragunova
+
+number = int(input("Please enter an 10 digit account number:"));
+'''
+numberX = (number6 + number4)
+print ();
+'''

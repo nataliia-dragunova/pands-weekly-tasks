94ac1b9687e317a592ea7bc899eda3c2afee1441
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-11 22:00:29 +0000

    task03

--- task03/accounts.py
+++ task03/accounts.py
@@ -4,9 +4,16 @@
 # the first 6 digits replaced with Xs
 # author : Nataliia Dragunova
 
-# first attempt - it works but it's clunky :) 
+# first attempt - it works but it's clunky because of spaces between numbers :) 
+# it's impossible to use this method to endless account number
+'''
+num = int(input("Please enter an 10 digit account number:"))
+acc = str(num)
+print("XXXXXX" + " "+ acc[6],acc[7],acc[8],acc[9]);
+'''
+# second attempt after watching the end of the 2d lecture
 # it's impossible to use this method to endless account number
 
 num = int(input("Please enter an 10 digit account number:"))
 acc = str(num)
-print("XXXXXX" + " "+ acc[6],acc[7],acc[8],acc[9]);
+print("XXXXXX" + acc[-4:10]); 

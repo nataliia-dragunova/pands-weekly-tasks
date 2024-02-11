ab89cebaea9031fe87f30e98f208514bb0dd642f
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-11 11:40:30 +0000

    task03

---
+++ task03/accounts.py
@@ -0,0 +1,5 @@
+# accounts.py
+# Program reads in a 10 character account number 
+# outputs the account number with only the last 4 digits showing
+# the first 6 digits replaced with Xs
+# author : Nataliia Dragunova

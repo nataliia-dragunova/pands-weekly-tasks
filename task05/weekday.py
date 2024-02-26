528dad21aeb5bec5ac1d11e698d56aa853b307f9
nataliia-dragunova <nataliia.dragunova@gmail.com>
2024-02-26 00:33:28 +0000

    changes in weekday

--- task05/weekday.py
+++ task05/weekday.py
@@ -4,9 +4,9 @@
 
 import datetime
 today = datetime.datetime.today() # today's date
-day_of_week = today.weekday() # the day of the week (Monday is 0, Sunday is 6)
+day_of_week = today.weekday() # the day of the week (Monday-Sunday is 0-6)
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"] # list of days of the week
-if days [-1:] or days[-2:]:
-    print("It is the weekend, yay!", days[day_of_week],":)")
+if days [-1:-3]:
+    print("It is the weekend, yay!", days[day_of_week],":)")    
 else:
     print("Yes, unfortunately today is a weekday.", days[day_of_week],":(");
